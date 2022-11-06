ec2a80ed0413c002ffab0e0c0fc46cd8df3c1a55
Yizhi Tang <tangyz@umich.edu>
2022-11-06 15:40:12 -0500

    a

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -20,6 +20,9 @@ def receive_from_end(endSocket):
     tmp_massage = ""
     while '\n' not in tmp_massage:
         tmp_massage = endSocket.recv(2048)
+        print(tmp_massage)
+        tmp_massage = tmp_massage.decode()
+        print(tmp_massage)
         # if the message is empty, there is a disconnection
         if tmp_massage == "":
             return (False, "")
