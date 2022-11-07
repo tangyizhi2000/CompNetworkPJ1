f71998ff507dc5e98c5aa88dd170a6afe2c52437
Yizhi Tang <tangyz@umich.edu>
2022-11-07 17:26:24 -0500

    receive func

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -142,13 +142,11 @@ def receive_from_end(endSocket, load):
         # what is header v.s. what is content
         end_of_header = temp_message.find(b'\r\n\r\n') + len(b'\r\n\r\n')
         temp_header = temp_message[:end_of_header].decode('utf-8', 'ignore')
-        print("!!", temp_message[:end_of_header])
         content_length = extract_content_length(temp_header) - (sys.getsizeof(temp_message[end_of_header:]) - sys.getsizeof(b''))
         while content_length > 0:
             temp_message = endSocket.recv(2048)
             end_of_header = temp_message.find(b'\r\n\r\n') + len(b'\r\n\r\n')
             temp_header = temp_message[:end_of_header].decode('utf-8', 'ignore')
-            print("!!", temp_message[:end_of_header], content_length)
             content_length = content_length - (sys.getsizeof(temp_message[end_of_header:]) - sys.getsizeof(b''))
             all_message = all_message + temp_message[end_of_header:]
         return (True, all_message)
