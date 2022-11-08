92506bd5f519f752a68ac63e57242a1d74e86c40
Yizhi Tang <tangyz@umich.edu>
2022-11-07 19:49:22 -0500

    can play video properly

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -175,6 +175,7 @@ def connect(recvSocket, fake_ip, web_server_ip):
                 status, response = time_and_send(serverSocket, client_messages, True)
                 # logging /bunny_1006743bps/BigBuckBunny_6s2.m4a
                 actual_chunk_name = re.findall('[.]*/bunny_[0-9]*bps/BigBuckBunny_6s[0-9]+\.m4s', client_messages.decode())
+                global log_list
                 log_list[-1] += (" " + str(int(actual_bitrate/1000)) + " " + str(web_server_ip) + " " + str(actual_chunk_name[0]))
                 print(log_list[-1])
                 if not status:
