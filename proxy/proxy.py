4f94e3e184188b0f2f0dec8a0c689f11348e3abf
Yizhi Tang <tangyz@umich.edu>
2022-11-08 14:48:03 -0500

    a

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -174,10 +174,10 @@ def connect(recvSocket, fake_ip, web_server_ip):
                 print("FINISHED MODIFYING REQUEST")
                 status, response = time_and_send(serverSocket, client_messages, True)
                 # logging /bunny_1006743bps/BigBuckBunny_6s_(init|[0-9]).mp4
-                print("client_messages.decode()")
                 actual_chunk_name = re.findall('[.]*/bunny_[0-9]*bps/BigBuckBunny_6s[0-9]+\.m4s', client_messages.decode())
+                if len(actual_chunk_name) == 0:
+                    print("client_messages.decode()", client_messages.decode()[:50])
                 global log_list
-                print("???", len(log_list), log_list[0], log_list[-1])
                 log_list[len(log_list)-1] += " " + str(int(actual_bitrate/1000)) + " " + str(web_server_ip) + " " + str(actual_chunk_name)
                 print(log_list[-1])
                 if not status:
