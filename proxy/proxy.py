06436ce7ceab7e8058119893dff29333a4025601
Yizhi Tang <tangyz@umich.edu>
2022-11-08 21:45:06 -0500

    not ok, change back, change -1

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -31,8 +31,11 @@ def time_and_send(serverSocket, client_message, video_chunk, client_IP, server_I
 def calculate_throughput(size, tf, ts, client_IP, server_IP):
     global T_current, alpha, log_list
     T = float((size - sys.getsizeof(b'')) / (tf - ts))
-    T_current[(client_IP, server_IP)] = alpha * T + (1 - alpha) * T_current[(client_IP, web_server_ip)]
-    log_list.append(str(time.time()) + " " + str(tf - ts) + " " + str(T/1000) + " " + str(T_current[(client_IP, web_server_ip)]/1000))
+    if T_current[(client_IP, server_IP)] == -1:
+        T_current[(client_IP, server_IP)] = bitrate_list[0]
+    else:
+        T_current[(client_IP, server_IP)] = alpha * T + (1 - alpha) * T_current[(client_IP, web_server_ip)]
+    log_list.append(str(time.time()) + " " + str(tf - ts) + " " + str(T) + " " + str(T_current[(client_IP, web_server_ip)]))
 
 # send a message to the target socket
 def send_to_end(endSocket, message):
